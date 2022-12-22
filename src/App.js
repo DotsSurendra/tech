93f43e1f247d8f67340afe74629e58f518afc7aa
DotsSurendra <surendra.parihar@dotsquares.com>
2022-12-22 10:38:27 +0530

    css array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
+import Data from "./Data";
 import Card from "./Card";
 
 function App(){
   return (<>
           <div className="Cards">
-            <Card  imgsrc='https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABY8mC90_Q2oSTzR-FOqzzehdI-U_XPF_qmRyWuQ82KtvzkF3kHyG7_DutjjcyPQG1n5yd1j2KEWzC5cejyKX9Iy9IOBzFKrRRKMj.jpg?r=f5a' ctitle='Doctor G' cdes="Medical student Uday Gupta grudgingly joins an all-women's gynecology class, but being a good doctor — and person — rests on pondering his prejudice" curl='https://www.netflix.com/in/title/81350339'/>
+            
   
-            <Card  imgsrc='https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABY8mC90_Q2oSTzR-FOqzzehdI-U_XPF_qmRyWuQ82KtvzkF3kHyG7_DutjjcyPQG1n5yd1j2KEWzC5cejyKX9Iy9IOBzFKrRRKMj.jpg?r=f5a' ctitle='Doctor G' cdes="Medical student Uday Gupta grudgingly joins an all-women's gynecology class, but being a good doctor — and person — rests on pondering his prejudice" curl='https://www.netflix.com/in/title/81350339'/>
-         
-          
-            <Card  imgsrc='https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABY8mC90_Q2oSTzR-FOqzzehdI-U_XPF_qmRyWuQ82KtvzkF3kHyG7_DutjjcyPQG1n5yd1j2KEWzC5cejyKX9Iy9IOBzFKrRRKMj.jpg?r=f5a' ctitle='Doctor G' cdes="Medical student Uday Gupta grudgingly joins an all-women's gynecology class, but being a good doctor — and person — rests on pondering his prejudice" curl='https://www.netflix.com/in/title/81350339'/>
+            {Data.map((val,key)=>{return(<Card  imgsrc={val.Dimg} ctitle={val.Dtitle} cdes={val.Ddes} curl={val.curl} key={key}/>)})}
+
+           
             </div>
           </>
   
---
+++ src/Data.jsx
@@ -0,0 +1,27 @@
+const Data=[
+    {
+        Dimg:'https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABY8mC90_Q2oSTzR-FOqzzehdI-U_XPF_qmRyWuQ82KtvzkF3kHyG7_DutjjcyPQG1n5yd1j2KEWzC5cejyKX9Iy9IOBzFKrRRKMj.jpg',
+        Dtitle:'Doctor G',
+        Ddes:"Medical student Uday Gupta grudgingly joins an all-women's gynecology class, but being a good doctor — and person — rests on pondering his prejudice",
+         curl:'https://www.netflix.com/in/title/81350339'
+    },
+    {
+        Dimg:'https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd1L2y9UjEsBIOPLEIkfkwsU6P25BKbOYDxueQfAjAAanaTPQ51MaTsQ3FWV_nBl2VvhfNgKnzO7dT30VahlzoXvEleQm_IltjEp.jpg',
+        Dtitle:'Bhool Bhulaiyaa 2',
+        Ddes:"When strangers Reet and Ruhan cross paths, their journey leads to an abandoned mansion and a dreaded spirit who has been trapped for 18 years.",
+         curl:'https://www.netflix.com/in/title/81243747'
+    },
+    {
+        Dimg:'https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaTAErs2J8-ecnu7c895rOdQ14oPSCUCc437oKRjUfVH_rzvwsg1vQdWQ4r93-zPwT1z7KnTaz0-T9TVxxSb_O2BpIvEScadm9gn.jpg',
+        Dtitle:'She',
+        Ddes:"An undercover assignment to expose a drug ring becomes a timid Mumbai constable’s road to empowerment as she realizes her dormant sexuality's potential.",
+         curl:'https://www.netflix.com/in/title/81183494'
+    },
+    {
+        Dimg:'https://occ-0-3997-2433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABQHoPiJI10fWHyAnUXDngS6VD2zjYYZ-D8lrBd8NY2gu81Pl4QG5hBJ_EGcwew53eDYVNyzvFOtSTE6WwdHaFicy6aqLZEWJAmo8.jpg',
+        Dtitle:'Goodbye',
+        Ddes:"After a mother's sudden death, chaos and grief collide when four adult siblings return to their traditional father's home for the funeral.",
+         curl:'https://www.netflix.com/in/title/81509545'
+    }
+];
+export default Data;
